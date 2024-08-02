02fce58175621233691355086d57ac6d8f1b9dcd
jose <jose.mondelo@iesdonana.org>
2024-08-02 09:29:21 +0200

    estilo boton perfil abierto listo

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -23,9 +23,9 @@ export default function Welcome({ auth }) {
                     {auth.user ? (
                         <Link
                             href={route('dashboard')}
-                            className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                            className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500 block text-center rounded-full px-6 py-2 bg-gray-100 border border-gray-300"
                         >
-                            Dashboard
+                            Perfil
                         </Link>
                     ) : (
                         <>
