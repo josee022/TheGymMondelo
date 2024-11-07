4ae8d0a71e483dcedc82907106ad4a690018ece2
josee022 <jose.mondelo@iesdonana.org>
2024-11-07 09:09:28 +0100

    cambio navegacion lista desplegable servicios

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -11,35 +11,47 @@ export default function Authenticated({ user = {}, header, children }) {
     return (
         <div className="min-h-screen bg-gray-100">
             <nav className="bg-black border-b border-gray-700 h-28">
-                {" "}
-                {/* Ajusta la altura aquí */}
                 <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-full">
-                    {/* Logo */}
                     <div className="flex items-center flex-shrink-0 h-full">
                         <Link href={route("inicio.index")}>
                             <img
                                 src="/imagenes/logo/1-logoWeb.png"
                                 alt="Logo"
-                                className="h-28 w-auto rounded-full" // Ajusta el logo para que ocupe toda la altura del contenedor
+                                className="h-28 w-auto rounded-full"
                             />
                         </Link>
                     </div>
-                    {/* Enlaces de navegación */}
+
                     <div className="flex-1 flex items-center justify-center space-x-8">
+                        <Dropdown>
+                            <Dropdown.Trigger>
+                                <button className="text-white text-xl relative group">
+                                    Servicios
+                                </button>
+                            </Dropdown.Trigger>
+                            <Dropdown.Content>
+                                <Dropdown.Link href={route("dietas.index")}>
+                                    Dietas
+                                </Dropdown.Link>
+                                <Dropdown.Link href={route("programas.index")}>
+                                    Programas
+                                </Dropdown.Link>
+                                <Dropdown.Link href={route("clases.index")}>
+                                    Clases
+                                </Dropdown.Link>
+                                <Dropdown.Link
+                                    href={route("suscripciones.index")}
+                                >
+                                    Suscripciones
+                                </Dropdown.Link>
+                            </Dropdown.Content>
+                        </Dropdown>
                         <NavLink
                             href={route("entrenadores.index")}
                             active={route().current("entrenadores.index")}
                             className="text-white text-xl relative group"
                         >
-                            Servicios
-                            <span
-                                className={`absolute inset-x-0 bottom-0 h-1 transition-transform transform ${
-                                    route().current("entrenadores.index")
-                                        ? "scale-x-100"
-                                        : "scale-x-0"
-                                }`}
-                                style={{ transformOrigin: "left" }}
-                            />
+                            Entrenadores
                         </NavLink>
                         <NavLink
                             href={route("tienda.index")}
@@ -47,14 +59,6 @@ export default function Authenticated({ user = {}, header, children }) {
                             className="text-white text-xl relative group"
                         >
                             Tienda
-                            <span
-                                className={`absolute inset-x-0 bottom-0 h-1 transition-transform transform ${
-                                    route().current("tienda.index")
-                                        ? "scale-x-100"
-                                        : "scale-x-0"
-                                }`}
-                                style={{ transformOrigin: "left" }}
-                            />
                         </NavLink>
                         <NavLink
                             href={route("blogs.create")}
@@ -62,14 +66,6 @@ export default function Authenticated({ user = {}, header, children }) {
                             className="text-white text-xl relative group"
                         >
                             Blog
-                            <span
-                                className={`absolute inset-x-0 bottom-0 h-1 transition-transform transform ${
-                                    route().current("blogs.create")
-                                        ? "scale-x-100"
-                                        : "scale-x-0"
-                                }`}
-                                style={{ transformOrigin: "left" }}
-                            />
                         </NavLink>
                         <NavLink
                             href={route("foros.index")}
@@ -77,14 +73,6 @@ export default function Authenticated({ user = {}, header, children }) {
                             className="text-white text-xl relative group"
                         >
                             Foro
-                            <span
-                                className={`absolute inset-x-0 bottom-0 h-1 transition-transform transform ${
-                                    route().current("foros.index")
-                                        ? "scale-x-100"
-                                        : "scale-x-0"
-                                }`}
-                                style={{ transformOrigin: "left" }}
-                            />
                         </NavLink>
                         <NavLink
                             href={route("diario.index")}
@@ -92,14 +80,6 @@ export default function Authenticated({ user = {}, header, children }) {
                             className="text-white text-xl relative group"
                         >
                             Diario
-                            <span
-                                className={`absolute inset-x-0 bottom-0 h-1 transition-transform transform ${
-                                    route().current("diario.index")
-                                        ? "scale-x-100"
-                                        : "scale-x-0"
-                                }`}
-                                style={{ transformOrigin: "left" }}
-                            />
                         </NavLink>
                         <NavLink
                             href={route("contacto")}
@@ -107,60 +87,34 @@ export default function Authenticated({ user = {}, header, children }) {
                             className="text-white text-xl relative group"
                         >
                             Contacto
-                            <span
-                                className={`absolute inset-x-0 bottom-0 h-1 transition-transform transform ${
-                                    route().current("contacto")
-                                        ? "scale-x-100"
-                                        : "scale-x-0"
-                                }`}
-                                style={{ transformOrigin: "left" }}
-                            />
                         </NavLink>
                     </div>
 
-                    {/* Menú de usuario */}
+                    {/* User Menu */}
                     <div className="flex items-center space-x-4">
-                        <div className="relative">
-                            <Dropdown>
-                                <Dropdown.Trigger>
-                                    <span className="inline-flex rounded-md">
-                                        <button
-                                            type="button"
-                                            className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-black bg-lime-400 hover:bg-lime-600 focus:outline-none transition ease-in-out duration-150"
-                                        >
-                                            {user.name || "Usuario"}
-                                            <svg
-                                                className="ms-2 h-4 w-4"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                viewBox="0 0 20 20"
-                                                fill="currentColor"
-                                            >
-                                                <path
-                                                    fillRule="evenodd"
-                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                                    clipRule="evenodd"
-                                                />
-                                            </svg>
-                                        </button>
-                                    </span>
-                                </Dropdown.Trigger>
-                                <Dropdown.Content>
-                                    <Dropdown.Link href={route("dashboard")}>
-                                        Perfil
-                                    </Dropdown.Link>
-                                    <Dropdown.Link
-                                        href={route("logout")}
-                                        method="post"
-                                        as="button"
-                                    >
-                                        Cerrar sesión
-                                    </Dropdown.Link>
-                                </Dropdown.Content>
-                            </Dropdown>
-                        </div>
+                        <Dropdown>
+                            <Dropdown.Trigger>
+                                <button className="inline-flex items-center px-3 py-2 border border-transparent text-sm font-medium rounded-md text-black bg-lime-400 hover:bg-lime-600">
+                                    {user.name || "Usuario"}
+                                </button>
+                            </Dropdown.Trigger>
+                            <Dropdown.Content>
+                                <Dropdown.Link href={route("dashboard")}>
+                                    Perfil
+                                </Dropdown.Link>
+                                <Dropdown.Link
+                                    href={route("logout")}
+                                    method="post"
+                                    as="button"
+                                >
+                                    Cerrar sesión
+                                </Dropdown.Link>
+                            </Dropdown.Content>
+                        </Dropdown>
                     </div>
                 </div>
-                {/* Menú móvil */}
+
+                {/* Mobile Menu */}
                 <div
                     className={
                         (showingNavigationDropdown ? "block" : "hidden") +
@@ -175,18 +129,6 @@ export default function Authenticated({ user = {}, header, children }) {
                             Inicio
                         </ResponsiveNavLink>
                         <ResponsiveNavLink
-                            href={route("entrenadores.index")}
-                            active={route().current("entrenadores.index")}
-                        >
-                            Entrenadores
-                        </ResponsiveNavLink>
-                        <ResponsiveNavLink
-                            href={route("clases.index")}
-                            active={route().current("clases.index")}
-                        >
-                            Clases
-                        </ResponsiveNavLink>
-                        <ResponsiveNavLink
                             href={route("dietas.index")}
                             active={route().current("dietas.index")}
                         >
@@ -199,16 +141,22 @@ export default function Authenticated({ user = {}, header, children }) {
                             Programas
                         </ResponsiveNavLink>
                         <ResponsiveNavLink
+                            href={route("clases.index")}
+                            active={route().current("clases.index")}
+                        >
+                            Clases
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink
                             href={route("suscripciones.index")}
                             active={route().current("suscripciones.index")}
                         >
                             Suscripciones
                         </ResponsiveNavLink>
                         <ResponsiveNavLink
-                            href={route("diario.index")}
-                            active={route().current("diario.index")}
+                            href={route("tienda.index")}
+                            active={route().current("tienda.index")}
                         >
-                            Diario
+                            Tienda
                         </ResponsiveNavLink>
                         <ResponsiveNavLink
                             href={route("blogs.create")}
@@ -223,10 +171,10 @@ export default function Authenticated({ user = {}, header, children }) {
                             Foro
                         </ResponsiveNavLink>
                         <ResponsiveNavLink
-                            href={route("tienda.index")}
-                            active={route().current("tienda.index")}
+                            href={route("diario.index")}
+                            active={route().current("diario.index")}
                         >
-                            Tienda
+                            Diario
                         </ResponsiveNavLink>
                         <ResponsiveNavLink
                             href={route("contacto")}
@@ -234,30 +182,6 @@ export default function Authenticated({ user = {}, header, children }) {
                         >
                             Contacto
                         </ResponsiveNavLink>
-                    </div>
-
-                    <div className="pt-4 pb-1 border-t border-gray-200">
-                        <div className="px-4">
-                            <div className="font-medium text-base text-gray-800">
-                                {user.name || "Invitado"}
-                            </div>
-                            <div className="font-medium text-sm text-gray-500">
-                                {user.email || "Sin correo"}
-                            </div>
-                        </div>
-
-                        <div className="mt-3 space-y-1">
-                            <ResponsiveNavLink href={route("dashboard")}>
-                                Perfil
-                            </ResponsiveNavLink>
-                            <ResponsiveNavLink
-                                method="post"
-                                href={route("logout")}
-                                as="button"
-                            >
-                                Cerrar sesión
-                            </ResponsiveNavLink>
-                        </div>
                     </div>
                 </div>
             </nav>
